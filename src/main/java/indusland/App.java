efb2250222e42fad3c0cdf7e8395f89f8c4e74ff
bharath.vishwanathula <bharathguptha60@gmail.com>
2022-06-25 20:57:00 +0530

    committed code for fundtrans

---
+++ src/main/java/indusland/App.java
@@ -0,0 +1,13 @@
+package indusland;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
